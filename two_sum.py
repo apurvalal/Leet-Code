bdf9a047fcdfd7ee6cf4e91b337dffc516434d06
Apurva <apurvalal2020@gmail.com>
2021-06-16 17:08:22 +0530

    2Sum Flash

--- two_sum.py
+++ two_sum.py
@@ -1,12 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int):
         solution = {}
-        for index in range(len(nums)):
-            solution[index] = nums[index]
-
-        for index in range(len(nums)):
-            if (target - nums[index]) in solution.values():
-                for key, value in solution.items():
-                    if value == (target - nums[index]) and key!=index:
-                        required_value = key
-                        return [index, key]
+        for index,value in enumerate(nums):
+            if (target - value) in solution:
+                return [solution[target - value], index]
+            else:
+                solution[value] = index
